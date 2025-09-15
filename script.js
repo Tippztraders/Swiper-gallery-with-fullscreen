2a2d2cbb59acc8e90ca6379a3ccfcecd193fc775
Tippztraders <tippzcashtraders@gmail.com>
2025-09-15 07:14:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function openSwiperModal(startIndex = 0) {
   const modal = document.querySelector('.swiper-modal');
   modal.style.display = 'flex';
 
-  // Init vertical swiper
+  // Init vertical swiper with mousewheel support
   mainSwiper = new Swiper('.main-swiper', {
     direction: 'vertical',
     spaceBetween: 10,
@@ -12,6 +12,7 @@ function openSwiperModal(startIndex = 0) {
       el: '.main-pagination',
       clickable: true,
     },
+    mousewheel: true, // allow desktop scroll
     initialSlide: startIndex,
   });
 }
@@ -25,3 +26,8 @@ function closeSwiperModal() {
     mainSwiper = null;
   }
 }
+
+// Extra: make sure both tap and mouse click open modal
+document.querySelectorAll('.featured-item').forEach((item, index) => {
+  item.addEventListener('click', () => openSwiperModal(index));
+});
